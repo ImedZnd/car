cd97d85caba698819c7127486c575d33bd10af99
ImdZnd <imedzenned@yahoo.fr>
2022-02-17 09:44:02 +0100

    skeleton project

---
+++ src/main/java/com/keyrus/pfe/imed/cleancarcrud/CleancarcrudApplication.java
@@ -0,0 +1,13 @@
+package com.keyrus.pfe.imed.cleancarcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleancarcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleancarcrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keyrus/pfe/imed/cleancarcrud/CleancarcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyrus.pfe.imed.cleancarcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleancarcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

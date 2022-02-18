cf52d3f0bbbf7ac6d6a0ff70cd7b5e3f35c2a1f9
ImdZnd <imedzenned@yahoo.fr>
2022-02-18 15:39:11 +0100

    car repository and the car repository error

---
+++ src/main/java/com/keyrus/pfe/imed/cleancarcrud/cleanworld/car/repository/CarRepository.java
@@ -0,0 +1,43 @@
+package com.keyrus.pfe.imed.cleancarcrud.cleanworld.car.repository;
+
+import com.keyrus.pfe.imed.cleancarcrud.cleanworld.car.model.Car;
+import io.vavr.control.Either;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface CarRepository {
+
+    Collection<Car> findAllCars();
+
+    Optional<Car> findCarByPlateNumber(String plateNumber);
+
+    Collection<Car> findAllCarsByType(String type);
+
+    Collection<Car> findAllCarsByReleaseYear(int releaseYear);
+
+    Either<? extends RepositoryCarError, Car> saveCar(Car car);
+
+    Either<? extends RepositoryCarError, Car> updateCar(Car car);
+
+    Optional<Car> deleteCar(Car car);
+
+    Optional<Car> deleteCar(String plateNumber);
+
+    sealed interface RepositoryCarError {
+
+        String message();
+
+        record CarWithPlateNumberAlreadyExistError(String message) implements RepositoryCarError {
+            public CarWithPlateNumberAlreadyExistError() {
+                this("");
+            }
+        }
+
+        record CarWithPlateNumberNotExistError(String message) implements RepositoryCarError {
+            public CarWithPlateNumberNotExistError() {
+                this("");}
+        }
+    }
+
+}

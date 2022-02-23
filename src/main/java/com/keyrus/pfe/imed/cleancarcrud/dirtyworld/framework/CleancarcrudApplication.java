cb1dcdab4dde83ded6c65be0a438ad5acc60eea2
ImdZnd <imedzenned@yahoo.fr>
2022-02-23 22:53:36 +0100

    Application load context test

--- src/main/java/com/keyrus/pfe/imed/cleancarcrud/dirtyworld/framework/CleancarcrudApplication.java
+++ src/main/java/com/keyrus/pfe/imed/cleancarcrud/dirtyworld/framework/CleancarcrudApplication.java
@@ -2,8 +2,10 @@ package com.keyrus.pfe.imed.cleancarcrud.dirtyworld.framework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.keyrus.pfe.imed.cleancarcrud.**")
 public class CleancarcrudApplication {
 
 	public static void main(String[] args) {

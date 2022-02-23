10b5ac77b4af7c626e501f477dc4043c6640b3df
ImdZnd <imedzenned@yahoo.fr>
2022-02-23 22:53:36 +0100

    Application load context test

--- src/test/java/com/keyrus/pfe/imed/cleancarcrud/dirtyworld/framework/CleancarcrudApplicationTest.java
+++ src/test/java/com/keyrus/pfe/imed/cleancarcrud/dirtyworld/framework/CleancarcrudApplicationTest.java
@@ -3,13 +3,11 @@ package com.keyrus.pfe.imed.cleancarcrud.dirtyworld.framework;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class CleancarcrudApplicationTest {
 
     @Test
-    public void contextLoad(){
+    void contextLoad(){
 
     }
 }

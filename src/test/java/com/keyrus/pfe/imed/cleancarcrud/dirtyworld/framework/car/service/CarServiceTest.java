f428208ddd0065470b6aa5c81c982aa00b53b75c
Imed ZnD <imedzenned@yahoo.fr>
2022-03-22 20:00:47 +0100

    enable test car service

--- src/test/java/com/keyrus/pfe/imed/cleancarcrud/dirtyworld/framework/car/service/CarServiceTest.java
+++ src/test/java/com/keyrus/pfe/imed/cleancarcrud/dirtyworld/framework/car/service/CarServiceTest.java
@@ -20,7 +20,6 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
-@Disabled
 @SpringBootTest
 @ContextConfiguration(initializers = {Initializer.class})
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
